12f197687be4442f68309decf7b9b3c09ec44070
Adam Kosinski <akosinski@paypoint.com>
2014-11-23 18:19:43 +0000

    initial commit

---
+++ XamarinSample/XamarinSample.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using XamarinSample.WinPhone.Resources;
+
+namespace XamarinSample.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
