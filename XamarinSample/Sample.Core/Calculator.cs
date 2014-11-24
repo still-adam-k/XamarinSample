7e948bd86bce4ab344b33e20b738105697a15922
Adam Kosinski <akosinski@paypoint.com>
2014-11-24 12:47:33 +0000

    attaching external project

---
+++ XamarinSample/Sample.Core/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Core
+{
+    public class Calculator
+    {
+        public string Add(int no1, int no2)
+        {
+            var sum = no1 + no2;
+            return sum.ToString();
+        }
+    }
+}
--- XamarinSample/XamarinSample/NextActivity.cs
+++ XamarinSample/XamarinSample/NextActivity.cs
@@ -9,6 +9,7 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Sample.Core;
 
 namespace XamarinSample
 {
@@ -18,9 +19,16 @@ namespace XamarinSample
         protected override void OnCreate(Bundle bundle)
         {
             SetContentView(Resource.Layout.Next);
-
+            
             base.OnCreate(bundle);
-            // Create your application here
+
+            var calculator = new Calculator();
+            var s1 = 1;
+            var s2 = 2;
+            var sum = calculator.Add(1, 2);
+
+            var label = FindViewById<TextView>(Resource.Id.textView1);
+            label.Text = "The sum of " + s1 + " and " + s2 + " is " + sum;
         }
     }
 }
